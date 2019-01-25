a5e85fd318aac072b60ced6fb17cf9c4a50f3d50
Jake Ryan <jake@subtextdigital.com>
2019-01-25 13:28:15 -0500

    clean up ignored files

--- wp-content/themes/ri-fusion/included/templates/search.php
+++
@@ -1,6 +0,0 @@
-<div class="search-wrap rit-search">
-    <form method="get" class="clearfix" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-        <input type="text" class="ipt text-field body-font" name="s" placeholder="<?php echo esc_html__('Type & hit enter...', 'ri-fusion'); ?>" autocomplete="off" />
-        <i class="fa fa-search"></i>
-    </form>
-</div>
